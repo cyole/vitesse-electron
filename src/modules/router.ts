37df2551292428dc010bad2359b9e3410e64a9ab
liyujie <hi@cyoo.cn>
2023-03-27 18:00:32 +0800

    :wrench: update router config

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -8,9 +8,6 @@ const routes = setupLayouts(generatedRoutes)
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
-  scrollBehavior() {
-    return { top: 0 }
-  },
 })
 
 export const install: UserModule = (app) => {
